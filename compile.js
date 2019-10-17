aa842dfd8ca88f88240efb6999f6926a18981f69
ponnea <1184875406@qq.com>
2019-10-17 18:11:52 +0800

    事件及S-

--- compile.js
+++ compile.js
@@ -26,7 +26,20 @@ class Compile {
         const childNodes = el.childNodes;
         Array.from(childNodes).forEach(node => {
             if(this.isElement(node)) {
-                console.log('编译元素'+node.nodeName);
+                // console.log('编译元素'+node.nodeName);
+                // s- : @
+                const nodeAttrs = node.attributes;
+                Array.from(nodeAttrs).forEach(attr => {
+                    const attrName = attr.name;//属性名
+                    const exp = attr.value;//属性值
+                    if (this.isDirective(attrName)) {
+                        const dir = attrName.substring(2);
+                        this[dir] && this[dir](node,this.$vm,exp);
+                    } else if(this.isEvent(attrName)) {
+                        const dir = attrName.substring(1);
+                        this.eventHandler(node,this.$vm,exp,dir)
+                    }
+                })
             } else if(this.isInterPolation(node)) {
                 this.compileText(node);
                 // console.log('编译文本'+node.textContent);
@@ -51,13 +64,32 @@ class Compile {
     // 代理更新方法
     update (node, vm, exp, dir) {
         const updateFn = this[dir+'Updater'];
+        console.log(exp)
         updateFn && updateFn(node,vm[exp]);
         new Watcher(vm, exp, function(value) {
             updateFn && updateFn(node,value);
         })
     }
+    //   事件处理器
+    eventHandler(node, vm, exp, dir) {
+        //   @click="onClick"
+        let fn = vm.$options.methods && vm.$options.methods[exp];
+        if (dir && fn) {
+        node.addEventListener(dir, fn.bind(vm));
+        }
+    }
     // 插值更新
     textUpdater (node, value) {
+        console.log(value)
         node.textContent = value
+    }
+    isDirective (attrName) {
+        return attrName.indexOf('s-') == 0;
+    }
+    text (node, vm, exp) {
+        this.update(node, vm, exp, 'text')
+    }
+    isEvent (attrName) {
+        return attrName.indexOf('@') == 0;
     }
 }
